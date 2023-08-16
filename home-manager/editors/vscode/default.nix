b5c1c8a5555ff7f363ae4d2110387c6a9860d34e
lcnbr <im@lcnbr.ch>
2023-08-16 09:59:38 +0200

    migrated config

--- home-manager/editors/vscode/default.nix
+++ home-manager/editors/vscode/default.nix
@@ -1,5 +1,5 @@
 {
-  inputs',
+  inputs,
   pkgs,
   ...
 }: let
@@ -14,9 +14,33 @@ in {
   programs.vscode = {
     enable = true;
 
-    # defaultEditor = true;
-    settings = {
-      theme = "catppuccin_latte";
+    enableExtensionUpdateCheck = false;
+    enableUpdateCheck = false;
+    extensions = with pkgs.vscode-extensions;
+      [
+        kamadorueda.alejandra
+        ms-python.python
+        rust-lang.rust-analyzer
+        eamodio.gitlens
+        bbenoist.nix
+        arrterian.nix-env-selector
+        denoland.vscode-deno
+        ritwickdey.liveserver
+        github.copilot
+        foam.foam-vscode
+        bmewburn.vscode-intelephense-client
+      ]
+      ++ marketplace-extensions;
+    userSettings = {
+      "terminal.integrated.defaultProfile.linux" = "fish";
+      "workbench.colorTheme" = "Semantic Lunaria Light"; #theme, want to modify it 22.5.23
+      "git.enableSmartCommit" = true; # autofetch and things
+      "git.confirmSync" = false;
+      "git.autofetch" = true;
+      "terminal.integrated.commandsToSkipShell" = [
+        "language-julia.interrupt"
+      ];
+      "julia.symbolCacheDownload" = true;
     };
   };
 }
